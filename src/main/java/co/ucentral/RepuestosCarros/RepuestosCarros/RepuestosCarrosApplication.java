aa6dc455b231d4622dccb3a20b45e014503b9ef4
Paula Torres <ptorresj@ucentral.edu.co>
2024-10-15 19:15:23 -0500

    Segundo Commit

---
+++ src/main/java/co/ucentral/RepuestosCarros/RepuestosCarros/RepuestosCarrosApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.RepuestosCarros.RepuestosCarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepuestosCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepuestosCarrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ucentral/RepuestosCarros/RepuestosCarros/RepuestosCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.RepuestosCarros.RepuestosCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepuestosCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
