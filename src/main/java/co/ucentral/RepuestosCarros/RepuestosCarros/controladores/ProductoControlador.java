da124e35f8493230e6e132455291ba3026ffc472
palita130611 <ptorresj@ucentral.edu.co>
2024-11-05 18:48:53 -0500

    (nuevo): se crean los metodos de la entidad Producto

--- src/main/java/co/ucentral/RepuestosCarros/RepuestosCarros/controladores/ProductoControlador.java
+++ src/main/java/co/ucentral/RepuestosCarros/RepuestosCarros/controladores/ProductoControlador.java
@@ -1,4 +1,18 @@
 package co.ucentral.RepuestosCarros.RepuestosCarros.controladores;
 
+import co.ucentral.RepuestosCarros.RepuestosCarros.persistencia.entidades.Producto;
+import co.ucentral.RepuestosCarros.RepuestosCarros.servicios.ProductoServicio;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Controller
 public class ProductoControlador {
+    ProductoServicio productoServicio;
+
+    public List<Producto> guardarProductos(){
+        return productoServicio.obtenerTodosLosProductos();
+    }
 }
--- src/main/java/co/ucentral/RepuestosCarros/RepuestosCarros/servicios/ProductoServicio.java
+++ src/main/java/co/ucentral/RepuestosCarros/RepuestosCarros/servicios/ProductoServicio.java
@@ -1,8 +1,26 @@
 package co.ucentral.RepuestosCarros.RepuestosCarros.servicios;
 
+import co.ucentral.RepuestosCarros.RepuestosCarros.persistencia.entidades.Producto;
+import co.ucentral.RepuestosCarros.RepuestosCarros.persistencia.repositorios.ProductoRepositorio;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@AllArgsConstructor
 @Service
 public class ProductoServicio {
+
+    ProductoRepositorio productoRepositorio; //la clase ProductoServicio depende de la interfaz ProductoRepositorio
+
+    //metodos
+    public Producto guardarproducto(Producto producto){
+        return productoRepositorio.save(producto); //llama el metodo save,save guarda el objeto producto en la BDA,retorna al obejto producto
+    }
+
+    public List<Producto> obtenerTodosLosProductos(){
+        List<Producto> listado = (List<Producto>) productoRepositorio.findAll();//la salida dindAll la convierte a una listado
+        return listado;
+    }
 
 }
